e3a239841dfc0e8626adbef8930fe7072d5793c9
Gabriela Farinha <vallefarinha@gmail.com>
2024-03-08 09:20:05 -0300

    add cards components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import CardComponent from "../components/card-component/CardComponent";
-
+import CardComponentQuantity from "../components/card-component/CardComponentQuantity"
 
 const Home = () => {
   return (
     <>    
     {/* <Banner/>   */}
     <CardComponent/>
+    <CardComponentQuantity/>
     </>
   )
 }

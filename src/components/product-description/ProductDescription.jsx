3b67f89ea4f54054eff67b70ad1649ff25308673
NathaRuiz <nruizm98@gmail.com>
2024-03-08 13:02:39 +0100

    update useState

--- src/components/product-description/ProductDescription.jsx
+++ src/components/product-description/ProductDescription.jsx
@@ -10,14 +10,14 @@ const ProductDescription = () => {
     price: 0.3,
   };
 
-  const [quantity, setQuantity] = useState(1);
+  const [quantity, setQuantity] = useState(0);
 
   const incrementQuantity = () => {
     setQuantity((prevQuantity) => prevQuantity + 1);
   };
 
   const decrementQuantity = () => {
-    if (quantity > 1) {
+    if (quantity > 0) {
       setQuantity((prevQuantity) => prevQuantity - 1);
     }
   };

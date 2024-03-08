63c39b15cdc19ce0c32e8661d098489962e40cfe
Desiree <desireesanchezdev@gmail.com>
2024-03-08 21:58:20 +0100

    Fix tw-react-element change to CDN

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import CardComponent from "../components/card-component/CardComponent";
 import CardComponentQuantity from "../components/card-component/CardComponentQuantity";
 // import CartComponent from "../components/cart-component/CartComponent";
-import Banner from "../components/banner/Banner";
+import CarouselBasicExample from "../components/banner/Banner.jsx"
+
+
 
 const Home = () => {
   return (
     <>    
-    <Banner/>
+    <CarouselBasicExample/>
     <CardComponent/>
     <CardComponentQuantity/>
     {/* <CartComponent/> */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,9 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/tw-elements/js/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  darkMode: "class",
-  plugins: [require("tw-elements/plugin.cjs")],
+  plugins: [],
 };

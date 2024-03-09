1dea60401753b4ff012991fd710f18fc18f35012
Gabriela Farinha <vallefarinha@gmail.com>
2024-03-09 12:37:00 -0300

    add images to banner

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,4 +1,7 @@
 import { TECarousel, TECarouselItem } from "tw-elements-react";
+import banner from "../../assets/images/banner.jpg";
+import ensalada from "../../assets/images/ensalada.jpg";
+import bowl from "../../assets/images/bowl.jpg";
 
 export default function CarouselBasicExample(){
   return (
@@ -10,48 +13,42 @@ export default function CarouselBasicExample(){
             className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
           >
             <img
-              src="https://tecdn.b-cdn.net/img/Photos/Slides/img%20(15).jpg"
-              className="block w-full"
-              alt="..."
+              src={banner}
+              className="block w-full h-72 object-cover"
+              alt="banner"
             />
-            <div className="absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-white md:block">
-              <h5 className="text-xl">First slide label</h5>
-              <p>
-                Some representative placeholder content for the first slide.
-              </p>
-            </div>
           </TECarouselItem>
           <TECarouselItem
             itemID={2}
             className="relative float-left hidden -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
           >
             <img
-              src="https://tecdn.b-cdn.net/img/Photos/Slides/img%20(22).jpg"
-              className="block w-full"
-              alt="..."
+              src={ensalada}
+              className="block w-full h-72 object-cover"
+              alt="ensalada"
             />
-            <div className="absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-white md:block">
+            {/* <div className="absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-gray md:block">
               <h5 className="text-xl">Second slide label</h5>
               <p>
                 Some representative placeholder content for the second slide.
               </p>
-            </div>
+            </div> */}
           </TECarouselItem>
           <TECarouselItem
             itemID={3}
             className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
           >
             <img
-              src="https://tecdn.b-cdn.net/img/Photos/Slides/img%20(23).jpg"
-              className="block w-full"
-              alt="..."
+              src={bowl}
+              className="block w-full h-72 object-cover"
+              alt="bowl"
             />
-            <div className="absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-white md:block">
+            {/* <div className="absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-gray md:block">
               <h5 className="text-xl">Third slide label</h5>
               <p>
                 Some representative placeholder content for the third slide.
               </p>
-            </div>
+            </div> */}
           </TECarouselItem>
         </div>
       </TECarousel>

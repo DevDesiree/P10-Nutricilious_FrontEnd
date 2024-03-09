fc193691a012c8832c24bd4bd6e739c38df7b6e3
NathaRuiz <nruizm98@gmail.com>
2024-03-09 21:14:40 +0100

    add route and create CompanyProductsTable

--- src/configuration/Router.jsx
+++ src/configuration/Router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Product from "../pages/Product";
 import Products from "../pages/Products";
+import CompanyView from "../pages/company-views/CompanyView";
 
 
 export default function Router() {
@@ -11,7 +12,8 @@ export default function Router() {
         <Route path="/" element={<Home/>}></Route> 
         <Route path="/product" element={<Product/>}></Route> 
         {/* <Route path="/cart" element={<Cart/>}></Route>  */}
-        <Route path="/products" element={<Products></Products>}></Route> 
+        <Route path="/products" element={<Products></Products>}></Route>
+        <Route path="/company/products" element={<CompanyView></CompanyView>}></Route>
     </Routes>
 </BrowserRouter>
   )
---
+++ src/pages/company-views/CompanyView.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CompanyProductsTable from '../../components/company-products-table/CompanyProductsTable'
+
+const CompanyView = () => {
+  return (
+    <div>
+      <CompanyProductsTable></CompanyProductsTable>
+    </div>
+  )
+}
+
+export default CompanyView

318090c245d7bd926c5a178550208ab682b7b0dc
Gabriela Farinha <vallefarinha@gmail.com>
2024-03-09 11:57:57 -0300

    new style to card

--- src/components/card-component/CardComponent.jsx
+++ src/components/card-component/CardComponent.jsx
@@ -2,19 +2,19 @@ import avocadoYTomate from '../../assets/images/avocadoYTomate.jpg';
 
 export default function CardComponent() {
   return (
-<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
+    <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
         <img className="rounded-t-lg" src={avocadoYTomate} alt="" />
-    </a>
-    <div className="p-5">
+      </a>
+      <div className="p-4">
         <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray text-left">Veganos</h5>
+          <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-gray text-left">Veganos</h5>
         </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-left">Descubre nuestros productos veganos: deliciosos y éticos. ¡Haz tu elección consciente hoy mismo!</p>
+        <p className="mb-3 text-gray-700 dark:text-gray-400 text-left">Descubre nuestros productos veganos: deliciosos y éticos. ¡Haz tu elección consciente hoy mismo!</p>
         <div className="text-left">
-        <a type="button" href="#" className="focus:outline-none text-gray bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Yo quiero!</a>
+          <a type="button" href="#" className="focus:outline-none text-gray bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Yo quiero!</a>
         </div>
-    </div>
-</div>
-  )
+      </div>
+    </div>    
+  );
 }

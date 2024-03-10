f03fedc838373a4cd9af6357eb50dc769f85c8b0
Oliwia Ann Koltun <144242610+LiwKltn@users.noreply.github.com>
2024-03-10 19:56:40 +0100

    added styles to navbar again

--- src/components/navbar-component/NavbarComponent.jsx
+++ src/components/navbar-component/NavbarComponent.jsx
@@ -4,13 +4,14 @@ export default function NavbarComponent() {
     return (
     
 <nav className="bg-black border-black-200 dark:bg-black-900">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src={logoNavbar} className="h-8" alt="Logo" />
-      <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Nutrilicious</span>
+  <div className="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-5">
+  <a href="" className="flex items-center space-x-5 rtl:space-x-reverse">
+      <img src={logoNavbar} className="h-20" alt="Logo" />
+      <span className="self-center text-3xl font-semibold whitespace-nowrap text-white">Nutrilicious</span>
   </a>
-  <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-      <button type="button" className="text-white bg-blue-700 hover:bg-amber-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-amber-400 dark:focus:ring-amber-400">Log In</button>
+  <div className="flex justify-between items-center ">
+    
+      <button type="button" className="text-black bg-amber-400 hover:bg-amber-400 focus:ring-4 focus:outline-none focus:ring-amber-400 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-amber-100 dark:hover:bg-amber-400 dark:focus:ring-amber-400">Log In</button>
       <button data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -21,22 +22,25 @@ export default function NavbarComponent() {
   <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
     <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-black-100 rounded-lg bg-black-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-black dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
       <li>
-        <a href="#" className="block py-2 px-3 md:p-0 text-amber-400 bg-amber-400 rounded md:bg-transparent md:text-white md:dark:text-amber-400" aria-current="page">Home</a>
+        <a href="#" className="block py-2 px-3 md:p-0 text-amber-400 bg-amber-400 rounded md:bg-transparent md:text-white md:dark:text-amber-400" aria-current="page">Alimentos veganos y saludables</a>
       </li>
       <li>
-        <a href="#" className="block py-2 px-3 md:p-0 text-white rounded hover:bg-amber-400 md:hover:bg-transparent md:hover:text-amber-400 md:dark:hover:text-amber-400 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+        <a href="#" className="block py-2 px-3 md:p-0 text-white rounded hover:bg-amber-400 md:hover:bg-transparent md:hover:text-amber-400 md:dark:hover:text-amber-400 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Proteínas vegetales</a>
       </li>
       <li>
-        <a href="#" className="block py-2 px-3 md:p-0 text-white rounded hover:bg-white  md:hover:bg-transparent md:hover:text-amber-400 d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+        <a href="#" className="block py-2 px-3 md:p-0 text-white rounded hover:bg-white  md:hover:bg-transparent md:hover:text-amber-400 d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Granos y cereales</a>
       </li>
       <li>
-        <a href="#" className="block py-2 px-3 md:p-0 text-white rounded hover:bg-amber-400 md:hover:bg-transparent md:hover:text-amber-400 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+        <a href="#" className="block py-2 px-3 md:p-0 text-white rounded hover:bg-amber-400 md:hover:bg-transparent md:hover:text-amber-400 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Snacks saludables</a>
       </li>
       <li>
-        <a href="#" className="block py-2 px-3 md:p-0 text-white rounded hover:bg-amber-400 md:hover:bg-transparent md:hover:text-amber-400 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+        <a href="#" className="block py-2 px-3 md:p-0 text-white rounded hover:bg-amber-400 md:hover:bg-transparent md:hover:text-amber-400 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Frutas y verduras</a>
       </li>
       <li>
-        <a href="#" className="block py-2 px-3 md:p-0 text-white rounded hover:bg-amber-400 md:hover:bg-transparent md:hover:text-amber-400 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+        <a href="#" className="block py-2 px-3 md:p-0 text-white rounded hover:bg-amber-400 md:hover:bg-transparent md:hover:text-amber-400 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Proteína animal</a>
+      </li>
+      <li>
+        <a href="#" className="block py-2 px-3 md:p-0 text-white rounded hover:bg-amber-400 md:hover:bg-transparent md:hover:text-amber-400 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Aceites y condimentos saludables</a>
       </li>
     </ul>
   </div>
